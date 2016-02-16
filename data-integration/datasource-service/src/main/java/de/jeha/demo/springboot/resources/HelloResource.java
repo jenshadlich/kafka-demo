21dfe991a622aabae38a6a3a73660b8da2565b6e
Jens Hadlich <jenshadlich@googlemail.com>
2016-02-16 08:37:01 +0100

    tidy up

--- data-integration/datasource-service/src/main/java/de/jeha/demo/springboot/resources/HelloResource.java
+++ data-integration/datasource-service/src/main/java/de/jeha/demo/springboot/resources/HelloResource.java
@@ -1,12 +1,8 @@
 package de.jeha.demo.springboot.resources;
 
 import de.jeha.demo.springboot.api.HelloResponse;
-import de.jeha.demo.springboot.dao.HelloDAO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
 
 /**
  * @author jenshadlich@googlemail.com
@@ -14,12 +10,9 @@ import java.io.IOException;
 @RestController
 public class HelloResource {
 
-    @Autowired
-    private HelloDAO helloDAO;
-
     @RequestMapping(value = "/hello", produces = "application/json")
-    public HelloResponse hello() throws IOException {
-        return new HelloResponse(helloDAO.getHelloString());
+    public HelloResponse hello() {
+        return new HelloResponse("Hello World!");
     }
 
 }
